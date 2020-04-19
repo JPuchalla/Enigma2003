0bee7ff971cdd19866d363f0049793653d4fa8be
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-04-18 21:45:14 -0600

    added character set test and number formatter test to offset test

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -9,13 +9,26 @@ class OffsetTest < Minitest::Test
     assert_instance_of Offset, @offset
   end
 
+  def test_character_set_returns_characters
+    expected = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+    assert_equal expected, @offset.character_set
+
+
+  end
+
   def test_current_date_argument
     Date.stubs(:today).returns(Date.new(2020, 04, 18))
     assert_equal "180420", @offset.date_set
   end
 
   def test_can_return_last_four_of_squared_date
-    # need to set up a mock here
-    assert_equal 6400, @offset.date_formatter
+    assert_equal ["6", "4", "0", "0"], @offset.date_last_four
   end
+
+  def test_it_can_return_hash_of_date_number_keys
+    expected = {:A=>6, :B=>4, :C=>0, :D=>0}
+    assert_equal expected, @offset.date_formatter
+  end
+
 end
