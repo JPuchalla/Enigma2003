ab6d779af4e353fbbb39371bd3e75675a2a52cf5
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-04-18 21:47:53 -0600

    created number_formatter method

--- lib/key.rb
+++ lib/key.rb
@@ -7,4 +7,14 @@ class Key
   def initialize(key = number_generator)
    @key = key
   end
+
+  def number_formatter
+    num_keys = Hash.new
+    num_keys[:A] = key[0..1].to_i
+    num_keys[:B] = key[1..2].to_i
+    num_keys[:C] = key[2..3].to_i
+    num_keys[:D] = key[3..4].to_i
+    return num_keys
+  end
+
 end
