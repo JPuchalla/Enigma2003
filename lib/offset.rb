3130916a09ec9fc9f0d461df392cbe341770684b
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-04-18 21:46:29 -0600

    created date_formatter method and helper method of date_last_four

--- lib/offset.rb
+++ lib/offset.rb
@@ -7,11 +7,20 @@ class Offset
    @date = date.to_i
   end
 
-  def date_formatter
-    (@date * @date).to_s[- 4 ,4].to_i
-
-    #logic
+  def date_last_four
+    date_four = (@date * @date).to_s[- 4 ,4]
+    date_four.chars
   end
 
+  def date_formatter
+    date_keys = Hash.new
+    date_keys[:A] = date_last_four[0].to_i
+    date_keys[:B] = date_last_four[1].to_i
+    date_keys[:C] = date_last_four[2].to_i
+    date_keys[:D] = date_last_four[3].to_i
+    date_keys
+  end
+
+  
 
 end
