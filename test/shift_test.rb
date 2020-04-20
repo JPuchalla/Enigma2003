f471228afe78e2f481e9ca08643dbae55ccad491
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-04-20 10:01:51 -0600

    test for character to number, split_message, and message index

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -12,11 +12,24 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, @shift
   end
 
-  def test_it_can_return_hash_of_total_offset
+  def test_it_can_return_hash_of_the_shifter
     expected = {:A=>18, :B=>27, :C=>34, :D=>45}
-    assert_equal expected, @shift.total_offset
+    assert_equal expected, @shift.shifter
   end
 
-  def test_if_it_can_shift
+  def test_can_convert_character_set_to_numbers
+    skip
+    expected = {"a"=>0,"b"=>1,"c"=>2,"d"=>3,"e"=>4,"f"=>5,"g"=>6,"h"=>7,"i"=>8,
+    "j"=>9,"k"=>10,"l"=>11,"m"=>12,"n"=>13,"o"=>14,"p"=>15,"q"=>16,"r"=>17,
+    "s"=>18,"t"=>19,"u"=>20,"v"=>21,"w"=>22,"x"=>23,"y"=>24, "z"=>25, " "=>26}
+    assert_equal expected, @shift.characters_to_numbers
+  end
+
+  def test_it_can_split_message
+    assert_equal ["h", "e", "l", "l", "o"], @shift.split_message("hello")
+  end
+
+  def test_message_index
+    assert_equal [7, 4, 11, 11, 14, 26], @shift.message_index("hello ")
   end
 end
