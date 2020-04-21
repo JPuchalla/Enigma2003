cc3325d6324feea4351c1470c40fb53825e7aa2c
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-04-21 02:42:11 -0600

    created test for enigma method encrypt (not currently functioning)

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,4 +8,15 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+#   def test_it_can_encrypt
+# skip
+#    expected = {encryption: 'keder ohulw!',
+#                key: '02715',
+#                date: '040895'}
+#
+#    assert_equal expected, @enigma.encrypt('Hello World!', '02715', '040895')
+#
+#   end
+
 end
